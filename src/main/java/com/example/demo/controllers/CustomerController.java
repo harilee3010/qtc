57e4d31e3fc569e4a676157828a88c26d97ca167
harilee3010 <hainaruto01@gmail.com>
2024-04-30 21:17:31 +0700

    UI

---
+++ src/main/java/com/example/demo/controllers/CustomerController.java
@@ -0,0 +1,12 @@
+package com.example.demo.controllers;
+
+import com.example.demo.services.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/customers")
+public class CustomerController {
+
+}
---
+++ src/main/java/com/example/demo/model/Customer.java
@@ -0,0 +1,94 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "Customers")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String address;
+    private String phone;
+    private String email;
+    private String position;
+    private String status;
+    private Date dataOff;
+    private String img;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getDataOff() {
+        return dataOff;
+    }
+
+    public void setDataOff(Date dataOff) {
+        this.dataOff = dataOff;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+}
